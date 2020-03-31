caa7fb6ef4ed7c460443bfb7a6b93dc75c6bce28
MorrisHu <wjhjb97@gmail.com>
2020-03-31 17:07:31 +0800

    ignore-lock

--- db/model/orderModel.js
+++ db/model/orderModel.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose')
 let orderSchema = new mongoose.Schema({
   orderCodes: { type:String ,required:true },
   userName: { type:String ,required:true },
+  userPhone: { type:String ,required:true },
   userAddr: { type:String ,required:true },
-  goodsCodes: { type:String ,required:true },
+  goodsInfos: { type:String ,required:true },
+  state: { type:String ,required:true },
   orderTime: { type:String ,required:true }
 })
 
--- router/orderRouter.js
+++ router/orderRouter.js
@@ -3,37 +3,40 @@ const router = express.Router()
 const {insertOrder,delOrder,findOrder} = require('../controls/orderControl')
 
 /**
- * @api {post} /admin/order/add   管理平台添加菜品
+ * @api {post} /order/add   添加订单
  * @apiName add
  * @apiGroup order
  *
- * @apiParam {String} name 订单名字.
- * @apiParam {String} price 菜品价格.
- * @apiParam {String} desc Users unique ID.
- * @apiParam {String} img Users unique ID.
- * @apiParam {Number} orderType Users unique ID.
+ * @apiParam {String} orderCodes 订单名字.
+ * @apiParam {String} userName 用户昵称.
+ * @apiParam {String} userPhone 用户手机.
+ * @apiParam {String} userAddr 用户地址.
+ * @apiParam {String} goodsInfos 商品信息.
+ * @apiParam {String} state 订单状态.
+ * @apiParam {String} orderTime 下单时间戳.
  *
  * @apiSuccess {String} err 状态码r.
  * @apiSuccess {String} msg  信息提示.
  */
 router.post('/add',(req,res)=>{
   // 接受数据
-  let {orderCodes,userName,userAddr,goodsCodes} = req.body 
+  let {orderCodes,userName,userPhone,userAddr,goodsInfos} = req.body 
   let orderTime = new Date()
+  let state = false
   console.log(req.body)
   // 处理数据 插入数据库
-  insertOrder({orderCodes,userName,userAddr,goodsCodes,orderTime})
+  insertOrder({orderCodes,userName,userPhone,userAddr,goodsInfos,state,orderTime})
   .then(()=>{res.send({err:0,msg:'插入成功'})})
   .catch((err)=>{
     res.send({err:-1,msg:'插入失败请重试'})})
   // 返回数据
 })
 /**
- * @api {post} /admin/order/getInfo  查询全部
+ * @api {post} /order/getInfo  查询全部或个人
  * @apiName getInfo
  * @apiGroup order
  *
- * @apiSuccess {String} err 状态码r.
+ * @apiSuccess {String} err 状态码.
  * @apiSuccess {String} msg  信息提示.
  * @apiSuccess {Array} list  查询到的数据.
  */
@@ -45,16 +48,14 @@ router.post('/getInfo',(req,res)=>{
   .catch((err)=>{res.send({err:-1,msg:'查询失败请重试'})})
 })
 /**
- * @api {post} /admin/order/del  订单删除
+ * @api {post} /order/del  订单删除
  * @apiName del
  * @apiGroup order
  *
- * @apiParam   {String} _id  菜品主键id
- * @apiSuccess {String} err 状态码r.
+ * @apiParam   {String} _id  订单主键id
+ * @apiSuccess {String} err 状态码.
  * @apiSuccess {String} msg  信息提示.
- * @apiSuccess {Array} list  查询到的数据.
  */
-// 2. 删除订单
 router.post('/del',(req,res)=>{
   // 获取要删除数据的id
   let {_id} = req.body
